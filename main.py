f1083e2be38e5eec9a29653fe12b620596cd7e14
Polos <85117866+RustemProg@users.noreply.github.com>
2023-11-30 01:18:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
